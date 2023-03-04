bdb2beeab7deb8f16a2e4d1586d0b1a4a8407c74
Arif Rachim <a.arif.r@gmail.com>
2023-03-04 19:25:51 +0400

    Upgrade tree

--- modules/iam/src/tree/DataTree.tsx
+++ modules/iam/src/tree/DataTree.tsx
@@ -1,13 +1,12 @@
-import {Store, StoreValueRenderer, useStore, useStoreListener, useStoreValue} from "../useStore";
+import {Store, StoreValueRenderer, useStore, useStoreListener} from "../useStore";
 import {TreeItem} from "../Database";
 import {Branch} from "../App";
-import {useEffect, useId, useState} from "react";
-import {motion} from "framer-motion";
-import {AiOutlineDelete} from "react-icons/ai";
+import {TreeRow} from "./TreeRow";
+import produce from "immer";
 
 
-type BranchWithLevel = Branch & { level: number };
-const border = '1px solid rgba(0,0,0,0.1)';
+export type BranchWithLevel = Branch & { level: number };
+export const border = '1px solid rgba(0,0,0,0.1)';
 
 export const rootRole: TreeItem = {
     name: 'root',
@@ -20,26 +19,28 @@ export function DataTree(props: {
     $treeData: Store<TreeItem[]>,
     $focusedItem: Store<TreeItem | undefined>,
     onItemChange: (id: string, value: Partial<TreeItem>) => void,
-    onItemDelete: (role: Branch) => void,
+    onItemDelete: (row: Branch) => void,
     onItemMove: (source: Branch, target: Branch) => void
 }) {
     const {$focusedItem, $treeData} = props;
-    const $rolesAndLevel = useStore<BranchWithLevel[]>([]);
+    const $rows = useStore<BranchWithLevel[]>([]);
     const $rowBeingDragHover = useStore<BranchWithLevel | undefined>(undefined);
     const $rowBeingDrag = useStore<BranchWithLevel | undefined>(undefined);
-    const $rolesTree = useStore<Branch>({
-        parentId: '',
-        parent: undefined,
-        name: rootRole.name,
-        id: rootRole.id,
-        children: [],
-        order: 1
-    });
+    const $toggleRows = useStore<BranchWithLevel[]>([]);
+    const $trunk = useStore<Branch | undefined>(undefined);
     useStoreListener($treeData, r => r, (roles) => {
-        const roleTree: Branch = constructBranch(roles);
-        const orderedRole = flatTree([roleTree], [], 0);
-        $rolesTree.set(roleTree);
-        $rolesAndLevel.set(orderedRole);
+        const trunk: Branch = constructTrunk(roles);
+        const orderedRole = flatTree($toggleRows.get(), [trunk],  0);
+        $trunk.set(trunk);
+        $rows.set(orderedRole);
+    })
+
+    useStoreListener($toggleRows, r => r, (toggleRows) => {
+        debugger;
+        const trunk = $trunk.get()!;
+        debugger;
+        const orderedRole = flatTree(toggleRows, [trunk],  0);
+        $rows.set(orderedRole);
     })
     return <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
         <div style={{display: 'flex', backgroundColor: '#F2F2F2', borderBottom: border, borderTop: border}}>
@@ -53,24 +54,38 @@ export function DataTree(props: {
                 Assigned Users
             </div>
         </div>
-        <StoreValueRenderer store={$rolesAndLevel} selector={s => s} render={(roles: BranchWithLevel[]) => {
+        <StoreValueRenderer store={$rows} selector={s => s} render={(rows: BranchWithLevel[]) => {
             return <div style={{height: '100%', backgroundColor: '#FEFEFE'}}>
-                {roles.filter(r => r.id !== rootRole.id).map((role, index) => {
-                    return <RenderTreeNode branch={role}
-                                           key={role.id}
-                                           onChange={(value) => {
-                                               props.onItemChange(role.id, {name: value});
-                                           }}
-                                           onDelete={(role: BranchWithLevel) => {
-                                               $focusedItem.set(undefined);
-                                               props.onItemDelete(role);
-                                           }}
-                                           onDrop={(draggedRole: BranchWithLevel) => {
-                                               props.onItemMove(draggedRole, role);
-                                           }}
-                                           $focusedItem={$focusedItem}
-                                           $itemBeingHovered={$rowBeingDragHover}
-                                           $itemBeingDragged={$rowBeingDrag}
+                {rows.filter(r => r.id !== rootRole.id).map((row) => {
+                    return <TreeRow row={row}
+
+                                    $toggleRows={$toggleRows}
+                                    key={row.id}
+                                    onChange={(value) => {
+                                        props.onItemChange(row.id, {name: value});
+                                    }}
+                                    onDelete={(row: BranchWithLevel) => {
+                                        $focusedItem.set(undefined);
+                                        props.onItemDelete(row);
+                                    }}
+                                    onDrop={(draggedRole: BranchWithLevel) => {
+                                        props.onItemMove(draggedRole, row);
+                                    }}
+                                    onToggleFolder={(row: BranchWithLevel) => {
+                                        $toggleRows.set(rows => {
+                                            const toggleRows = [...rows];
+                                            const index = toggleRows.findIndex(r => r.id === row.id);
+                                            if (index >= 0) {
+                                                toggleRows.splice(index, 1);
+                                            } else {
+                                                toggleRows.push(row);
+                                            }
+                                            return toggleRows;
+                                        });
+                                    }}
+                                    $focusedItem={$focusedItem}
+                                    $itemBeingHovered={$rowBeingDragHover}
+                                    $itemBeingDragged={$rowBeingDrag}
 
                     />
                 })}
@@ -79,7 +94,7 @@ export function DataTree(props: {
     </div>;
 }
 
-const constructBranch = (roles: TreeItem[]): Branch => {
+const constructTrunk = (roles: TreeItem[]): Branch => {
 
     const tree: Branch = {
         parentId: '',
@@ -127,7 +142,8 @@ const constructBranch = (roles: TreeItem[]): Branch => {
 }
 
 
-function flatTree(tree: Branch[], result: BranchWithLevel[], level: number) {
+function flatTree(toggleRows: Branch[], tree: Branch[],  level: number) {
+    let result: BranchWithLevel[] = [];
     tree.sort((a, b) => a.order - b.order).forEach((leaf: Branch) => {
         result.push({
             name: leaf.name,
@@ -138,110 +154,13 @@ function flatTree(tree: Branch[], result: BranchWithLevel[], level: number) {
             children: leaf.children,
             parent: leaf.parent
         });
-        const trees = flatTree(leaf.children, [], level + 1);
-        result = result.concat(trees);
+        const isClosed = toggleRows.findIndex(r => r.id === leaf.id) >= 0;
+        if (!isClosed) {
+            const trees = flatTree(toggleRows, leaf.children, level + 1);
+            result = result.concat(trees);
+        }
     });
     return result;
-}
-
-function RenderTreeNode(props: {
-    branch: BranchWithLevel,
-    onChange: (value: string) => void,
-    onDelete: (role: BranchWithLevel) => void,
-    onDrop: (role: BranchWithLevel) => void,
-    $focusedItem: Store<TreeItem | undefined>,
-    $itemBeingHovered: Store<BranchWithLevel | undefined>,
-    $itemBeingDragged: Store<BranchWithLevel | undefined>
-}) {
-
-    const {branch, onChange, $focusedItem, $itemBeingHovered, $itemBeingDragged, onDelete, onDrop} = props;
-    const [edit, setEdit] = useState<boolean>(true);
-    const id = useId();
-    const isFocused = useStoreValue($focusedItem, param => {
-        return param && branch ? param.id === branch.id : false
-    }, []);
-    const isBeingDrag = useStoreValue($itemBeingDragged, param => {
-        return param && branch ? param.id === branch.id : false
-    }, [])
-    const isDragHover = useStoreValue($itemBeingHovered, param => {
-        const rowBeingDrag = $itemBeingDragged.get();
-        if (param && rowBeingDrag && param.id === rowBeingDrag.id) {
-            return false;
-        }
-        return param && branch ? param.id === branch.id : false
-    }, []);
-    useEffect(() => {
-        function onClick() {
-            if (edit) {
-                const input: HTMLInputElement = document.getElementById(id)! as HTMLInputElement;
-                onChange(input.value);
-                setEdit(false);
-            }
-        }
-
-        window.addEventListener('click', onClick);
-        return () => {
-            window.removeEventListener('click', onClick);
-        }
-    }, [edit]);
-
-    return <motion.div style={{
-        opacity: isBeingDrag ? 0.2 : 1,
-        minHeight: 22,
-        display: 'flex',
-        flexDirection: 'column',
-        paddingLeft: 10 * branch.level,
-        backgroundColor: isDragHover ? 'yellow' : isFocused ? '#EEEEEE' : '#FFFFFF',
-        borderBottom: border
-    }}
-                       onDoubleClick={() => {
-                           setEdit(true);
-                       }}
-                       onClick={(event) => {
-                           $focusedItem.set(branch);
-                           if (edit) {
-                               event.stopPropagation();
-                               event.preventDefault();
-                           }
-                       }}
-                       draggable={true}
-                       onDragOver={(event) => {
-                           event.preventDefault();
-                           $itemBeingHovered.set(branch);
-                       }}
-                       onDragLeave={() => {
-                           if ($itemBeingHovered.get() === branch) {
-                               $itemBeingHovered.set(undefined);
-                           }
-                       }}
-                       onDrop={() => {
-                           onDrop($itemBeingDragged.get()!)
-                       }}
-                       onDragEnd={() => {
-                           $itemBeingHovered.set(undefined);
-                           $itemBeingDragged.set(undefined);
-                       }}
-                       onDragStart={() => {
-                           $itemBeingDragged.set(branch)
-                       }}
-    >
-        {edit && <input id={id} type="text" defaultValue={branch.name} autoFocus={true}
-                        style={{border: "none", padding: '3px 5px'}}/>}
-        {!edit && <div style={{display: 'flex'}}>{branch.name}
-            <div style={{flexGrow: 1}}/>
-            <div style={{
-                cursor: "pointer",
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                paddingRight: '10px'
-            }} onClick={async (event) => {
-                event.preventDefault();
-                event.stopPropagation();
-                onDelete(branch);
-            }}><AiOutlineDelete style={{fontSize: 18}}/></div>
-        </div>}
-    </motion.div>;
 }
 
 
---
+++ modules/iam/src/tree/TreeRow.tsx
@@ -0,0 +1,138 @@
+import {Store, useStoreValue} from "../useStore";
+import {TreeItem} from "../Database";
+import {useEffect, useId, useState} from "react";
+import {motion} from "framer-motion";
+import {AiOutlineDelete} from "react-icons/ai";
+import {border, BranchWithLevel} from "./DataTree";
+import {Visible} from "../utils/View";
+import {IoFolderOpenOutline, IoFolderOutline} from "react-icons/all";
+
+
+export function TreeRow(props: {
+    row: BranchWithLevel,
+    onChange: (value: string) => void,
+    onDelete: (row: BranchWithLevel) => void,
+    onDrop: (row: BranchWithLevel) => void,
+    onToggleFolder: (row: BranchWithLevel) => void,
+    $focusedItem: Store<TreeItem | undefined>,
+    $itemBeingHovered: Store<BranchWithLevel | undefined>,
+    $itemBeingDragged: Store<BranchWithLevel | undefined>,
+    $toggleRows: Store<BranchWithLevel[]>
+}) {
+
+    const {
+        row,
+        onChange,
+        $focusedItem,
+        $itemBeingHovered,
+        $itemBeingDragged,
+        onDelete,
+        onDrop,
+        onToggleFolder,
+        $toggleRows
+    } = props;
+    const [edit, setEdit] = useState<boolean>(true);
+    const id = useId();
+    const isFocused = useStoreValue($focusedItem, param => {
+        return param && row ? param.id === row.id : false
+    }, []);
+    const isBeingDrag = useStoreValue($itemBeingDragged, param => {
+        return param && row ? param.id === row.id : false
+    }, [])
+    const isDragHover = useStoreValue($itemBeingHovered, param => {
+        const rowBeingDrag = $itemBeingDragged.get();
+        if (param && rowBeingDrag && param.id === rowBeingDrag.id) {
+            return false;
+        }
+        return param && row ? param.id === row.id : false
+    }, []);
+    useEffect(() => {
+        function onClick() {
+            if (edit) {
+                const input: HTMLInputElement = document.getElementById(id)! as HTMLInputElement;
+                onChange(input.value);
+                setEdit(false);
+            }
+        }
+
+        window.addEventListener('click', onClick);
+        return () => {
+            window.removeEventListener('click', onClick);
+        }
+    }, [edit]);
+    const hasChildren = row.children && row.children.length > 0;
+    const isClosed = $toggleRows.get().findIndex(s => s.id === row.id) >= 0
+    return <motion.div style={{
+        opacity: isBeingDrag ? 0.2 : 1,
+        minHeight: 22,
+        display: 'flex',
+        flexDirection: 'column',
+        paddingLeft: 20 * row.level,
+        backgroundColor: isDragHover ? 'yellow' : isFocused ? '#EEEEEE' : '#FFFFFF',
+        borderBottom: border
+    }}
+                       onDoubleClick={() => {
+                           setEdit(true);
+                       }}
+                       onClick={(event) => {
+                           $focusedItem.set(row);
+                           if (edit) {
+                               event.stopPropagation();
+                               event.preventDefault();
+                           }
+                       }}
+                       draggable={true}
+                       onDragOver={(event) => {
+                           event.preventDefault();
+                           $itemBeingHovered.set(row);
+                       }}
+                       onDragLeave={() => {
+                           if ($itemBeingHovered.get() === row) {
+                               $itemBeingHovered.set(undefined);
+                           }
+                       }}
+                       onDrop={() => {
+                           onDrop($itemBeingDragged.get()!)
+                       }}
+                       onDragEnd={() => {
+                           $itemBeingHovered.set(undefined);
+                           $itemBeingDragged.set(undefined);
+                       }}
+                       onDragStart={() => {
+                           $itemBeingDragged.set(row)
+                       }}
+    >
+        <Visible if={edit}>
+            <input id={id} type="text" defaultValue={row.name} autoFocus={true}
+                   style={{border: "none", padding: '3px 5px'}}/>
+        </Visible>
+        <Visible if={!edit}>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'content', marginRight: 5}}
+                     onClick={() => {
+                         onToggleFolder(row);
+                     }}>
+                    <Visible if={!isClosed}>
+                        <IoFolderOpenOutline style={{fontSize: 20}}/>
+                    </Visible>
+                    <Visible if={isClosed}>
+                        <IoFolderOutline style={{fontSize: 20}}/>
+                    </Visible>
+                </div>
+                <div style={{flexGrow: 1}}>{row.name}</div>
+
+                <div style={{
+                    cursor: "pointer",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    paddingRight: '10px'
+                }} onClick={async (event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    onDelete(row);
+                }}><AiOutlineDelete style={{fontSize: 18}}/></div>
+            </div>
+        </Visible>
+    </motion.div>;
+}
